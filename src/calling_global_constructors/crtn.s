951fffb1fec3530b934866e00f27201032e87d62
TornaxO7 <tornax07@gmail.com>
2021-03-21 14:57:13 +0100

    Retry of the kernel

--- src/calling_global_constructors/crtn.s
+++
@@ -1,9 +0,0 @@
-.section .init
-    // gcc wiill put the content of crtend.o .init section here
-    popl  %ebp
-    ret
-
-.section .fini
-    // gcc will put the content of crtend.o's .fini section here
-    popl  %ebp
-    ret
